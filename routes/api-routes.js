7aff786f1eacd63251ca45ba227398803b46b140
SirPotatoIV <otoolej33@gmail.com>
2019-12-20 13:46:39 -0600

    Added stuff instructor recommended.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,17 @@
+const db = require("../models")
+
 module.exports = function(app){
     // use app to setup api routes
 
     //route to create a todo
-    app.post("/api/todo/", function(req, res){
-        const todo = db.Todo.create({task: req.body.task})
+    app.post("/api/todo/", async function(req, res){
+        const todo = await db.Todo.create({task: req.body.task})
         res.json(todo);
+    });
+
+    app.get("/api/todo", (req,res)=> {
+        db.Todo.findAll({}).then(todos => {
+            res.json(todos)
+        })
     })
 }
